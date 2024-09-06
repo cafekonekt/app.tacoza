82c8a997897d9f39f45c6e4ff527a8889fe3496d
Jatin <jatin.kshatriya2821@gmail.com>
2024-09-06 15:12:10 +0530

    added filter and interactive search

--- src/app/[menu]/order/[order_id]/page.js
+++ src/app/[menu]/order/[order_id]/page.js
@@ -150,7 +150,7 @@ export default function Order({ params }) {
         <CardHeader className="flex flex-row items-start bg-muted/50">
           <div className="grid gap-0.5">
             <CardTitle className="group flex items-center gap-2 text-lg">
-              Order {params.order_id}
+              Order {params.order_id.split("-")[0]}
               <Button
                 size="icon"
                 variant="outline"
